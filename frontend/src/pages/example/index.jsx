581e7454498220e22063bd7c0b9fb74e0013f0dc
juliokiyoshi <julio.k.r.matsoui@gmail.com>
2021-04-24 16:10:35 -0300

    add esqueleto do front-end

---
+++ frontend/src/pages/example/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { ExampleContainer, ExampleCounterButton } from "./styles";
+
+const ExamplePage = () => {
+  const [counter, setCounter] = useState(0);
+  const [isAdmin, setIsAdmin] = useState(true);
+  const counterClickHandler = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    //   Example Container é uma div definida no arquivo styles, criamos um componente para o javascript mas no dom vira uma div
+    <ExampleContainer>
+      <span> Você clicou {counter} vezes </span>
+      {/* Mesma coisa que o example container só que com um button, aqui temos um exemplo de como passar uma prop, basta abrir o styles para entender onde usamos essa props */}
+      <ExampleCounterButton
+        bdColor="#d4d432"
+        textColor="#000"
+        onClick={counterClickHandler}
+        isAdmin={isAdmin}
+        disabled={!isAdmin}
+      >
+        Clique aqui
+      </ExampleCounterButton>
+    </ExampleContainer>
+  );
+};
+export default ExamplePage;
