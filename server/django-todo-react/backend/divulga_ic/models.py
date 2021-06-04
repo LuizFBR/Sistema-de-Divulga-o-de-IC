dcaf9daf89f5724cdd630cf2211e5fa74f999f05
João Pedro Vianini de Paula <j176241@dac.unicamp.br>
2021-06-04 17:59:36 -0300

    Atualização de models.py para as novas classes

--- server/django-todo-react/backend/divulga_ic/models.py
+++ server/django-todo-react/backend/divulga_ic/models.py
@@ -1,51 +1,31 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
-class Aluno(models.Model):
-    RA = models.CharField(max_length=6)
-    nome = models.CharField(max_length=50)
+
+class Advisor(models.Model):    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.CharField(max_length=50)
-    curriculo = models.TextField()
-    integralizacao = models.TextField()
-    curso = models.CharField(max_length= 60)
-
-
-    def _str_(self):
-        return self.title
-
-
-class Professor(models.Model):    
-    email_institucional = models.CharField(max_length=50)
-    nome = models.CharField(max_length=50)
-    instituto = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    pagina = models.CharField(max_length=50)
-    universidade = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+    website = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
 
     def _str_(self):
         return self.title
 
-class Proposta(models.Model):    
-    aluno = models.CharField(max_length=50)
-    professor = models.CharField(max_length=50)
-    proposta = models.TextField()
-    data_proposta = models.DateTimeField(auto_now_add=False)
-    data_vigente = models.DateTimeField(auto_now_add=False)
 
-    def _str_(self):
-        return self.title
-
-class Divulgacao(models.Model):    
-    email_prof = models.CharField(max_length=50)
-    ic_id = models.DecimalField(decimal_places=2,max_digits=4)
-    tem_bolsa = models.BooleanField()
-    valor_bolsa = models.DecimalField(decimal_places=2,max_digits=4)
-    area = models.CharField(max_length=50)
-    descricao = models.TextField()
-    carga_horaria = models.DecimalField(decimal_places=2,max_digits=4)
-    data_divulgacao = models.DateTimeField()
-    data_vigente = models.DateTimeField()
+class IC(models.Model):    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    advisor = Advisor()
+    endDate = models.DateTimeField()
+    has_Scholarship = models.BooleanField()
+    scholarship_amount = models.DecimalField(decimal_places=2,max_digits=4)
+    subject = models.CharField(max_length=50)
+    requirements = ListCharFields(models.CharField(max_lenght=50),20)
+    workload = models.DecimalField(decimal_places=2,max_digits=4)
 
     def _str_(self):
         return self.title
