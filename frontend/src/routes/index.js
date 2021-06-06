953fb0ea14a73e5a8fe4714ce6bcd68fd55a3db6
Lucas <lucascarmo6@gmail.com>
2021-06-06 18:25:01 -0300

    Avaliação  5.2 - codes smell: Dispensables (front)

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import ExamplePage from "../pages/example";
-import LoginPage from "../pages/login";
-import PageIC from "../pages/exemploIC";
-
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={ExamplePage} />
-    <Route path="/login" component={LoginPage} />
-    <Route path="/PageIC" component={PageIC} />
   </Switch>
 );
 
