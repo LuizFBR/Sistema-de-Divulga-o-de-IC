1e1daf584dd3070f3af1eacb4086a6b1f7dab7f6
juliokiyoshi <julio.k.r.matsoui@gmail.com>
2021-07-09 19:16:12 -0300

    update CardIc and Header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
-import clsx from 'clsx';
-import AppBar from '@material-ui/core/AppBar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { Container, CustomToolbar } from './styles';
-import { InputBase, makeStyles, fade, SwipeableDrawer } from '@material-ui/core';
+import React, { useState } from "react";
+import clsx from "clsx";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Container, CustomToolbar } from "./styles";
+import {
+  InputBase,
+  makeStyles,
+  fade,
+  SwipeableDrawer,
+} from "@material-ui/core";
 
-
-
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,47 +29,47 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
-      width: 'auto',
+      width: "auto",
     },
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit",
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
   },
@@ -75,13 +77,11 @@ const useStyles = makeStyles((theme) => ({
     width: 250,
   },
   fullList: {
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-
 export default function Header() {
-
   const classes = useStyles();
   const [timeOut, setTimeOut] = useState(0);
   const [showDrawer, setShowDrawer] = useState(false);
@@ -90,68 +90,80 @@ export default function Header() {
     const inputValue = event.target.value;
 
     if (timeOut) clearTimeout(timeOut);
-    setTimeOut(setTimeout(() => {
-      console.log(inputValue)
+    setTimeOut(
+      setTimeout(() => {
+        console.log(inputValue);
 
-      // TODO Handle search ICs
-    }, 500));
-  }
+        // TODO Handle search ICs
+      }, 500)
+    );
+  };
 
   const openDrawer = () => setShowDrawer(true);
   const closeDrawer = () => setShowDrawer(false);
 
-
-  const renderDrawerContent = () => <div
-    className={clsx(classes.list, {
-      [classes.fullList]: 'left',
-    })}
-    role="presentation"
-    onClick={closeDrawer}
-    onKeyDown={closeDrawer}
-  >
-    <List>
-      {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-        <ListItem button key={text}>
-          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
-    <Divider />
-    <List>
-      {['All mail', 'Trash', 'Spam'].map((text, index) => (
-        <ListItem button key={text}>
-          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
-  </div>;
+  const renderDrawerContent = () => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: "left",
+      })}
+      role="presentation"
+      onClick={closeDrawer}
+      onKeyDown={closeDrawer}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
-
-    <Container>
-      <AppBar position="static" style={{ backgroundColor: '#000', color: '#fff' }} >
-        <CustomToolbar >
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => openDrawer()}>
+    <Container data-testid="home-page-header">
+      <AppBar
+        position="static"
+        style={{ backgroundColor: "#000", color: "#fff" }}
+      >
+        <CustomToolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => openDrawer()}
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6">
-            UniIC
-          </Typography>
+          <Typography variant="h6">UniIC</Typography>
           <InputBase
             placeholder="Search…"
             classes={{
               root: classes.inputRoot,
               input: classes.inputInput,
             }}
-            inputProps={{ 'aria-label': 'search' }}
+            inputProps={{ "aria-label": "search" }}
             onChange={handleInput}
           />
         </CustomToolbar>
       </AppBar>
       <SwipeableDrawer
-        anchor='left'
+        anchor="left"
         open={showDrawer}
         onClose={closeDrawer}
         onOpen={openDrawer}
@@ -159,6 +171,5 @@ export default function Header() {
         {renderDrawerContent()}
       </SwipeableDrawer>
     </Container>
-
   );
 }
